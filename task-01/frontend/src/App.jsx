7b54be65831636942232d1b248bf0c490e979769
Arunpragash22 <vimalanarunpragash2000@gmail.com>
2026-09-15 12:59:11 +0530

    Prepare both frontends for Vercel deployment

--- task-01/frontend/src/App.jsx
+++ task-01/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API = "http://100.48.83.254:8080";
+const API = "/api";
 
 function App() {
   const [products, setProducts] = useState([]);
